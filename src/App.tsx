685254f72fc794fc1352d3b0879911fe97cdb99a
Mykhailo Ostrovskyi <dmakgregor@gmail.com>
2023-02-09 17:39:12 +0200

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Layout from './components/layout/Layout';
-import { Editor, Preview } from './features/markdown';
+import {Editor, Preview} from './features/markdown';
 
 const App = () => (
   <Layout>
-    <Editor />
-    <Preview />
+    <Editor/>
+    <Preview/>
   </Layout>
 );
 
